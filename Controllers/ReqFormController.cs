d00364d67d2fce335713cc0ad85bc91ee1d07bff
shubham vegad <shubham.vegad@etatvasoft.com>
2024-02-05 18:29:33 +0530

    submit request form added

---
+++ Controllers/ReqFormController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hallocDoc.Controllers
+{
+    public class ReqFormController : Controller
+    {
+        public IActionResult Business()
+        {
+            return View();
+        }
+        public IActionResult Concierge()
+        {
+            return View();
+        }
+        public IActionResult family()
+        {
+            return View();
+        }
+        public IActionResult Patient()
+        {
+            return View();
+        }
+        public IActionResult SubmitReq()
+        {
+            return View();
+        }
+    }
+}
