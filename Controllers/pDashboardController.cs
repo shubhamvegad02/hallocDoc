db2a5ddb2dfcf62e52175196bc3a1badeae7ffa0
shubham vegad <shubham.vegad@etatvasoft.com>
2024-02-09 18:34:21 +0530

    patient dashboard added

---
+++ Controllers/pDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hallocDoc.Controllers
+{
+    public class pDashboardController : Controller
+    {
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
