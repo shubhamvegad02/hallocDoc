c03349f0380eadde1aef2b44127c65c1929d1bf1
shubham vegad <shubham.vegad@etatvasoft.com>
2024-02-23 18:35:23 +0530

    dashboard started

---
+++ hallocDoc/Controllers/ADashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hallocDoc.Controllers
+{
+    public class ADashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dmain()
+        {
+            return View();
+        }
+    }
+}
